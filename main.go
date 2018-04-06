9170cbd1e416e9b53625d1a31f9472103fd1c029
Sean Marciniak <sean@beamery.com>
2018-04-06 20:07:27 +0100

    Adding basic example of hello-world as the desired container

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"os"
+)
 
+func main() {
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		fmt.Println("We have an issue here: ", err)
+		os.Exit(1)
+	}
+	resp, err := cli.ContainerExecCreate(context.Background(), "hello-world", types.ExecConfig{
+		AttachStderr: true,
+		AttachStdout: true,
+	})
+	if err != nil {
+		fmt.Println("Today is a day of sadness:", err)
+		os.Exit(1)
+	}
+	_ = resp
 }
