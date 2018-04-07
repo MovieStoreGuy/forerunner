82a544d9da0a97030b33eee01e7d00b1675b4dfa
Sean Marciniak <sean@beamery.com>
2018-04-07 16:30:20 +0100

    Making some awesome headway with Forerunner

--- main.go
+++ main.go
@@ -1,42 +1,28 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"os"
-	"time"
 
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/container"
-	"github.com/docker/docker/client"
+	"github.com/MovieStoreGuy/forerunner/runner"
 )
 
 func main() {
-	cli, err := client.NewEnvClient()
+	mchief, err := runner.New(nil)
 	if err != nil {
-		fmt.Println("We have an issue here: ", err)
+		fmt.Println("An issued occured\n\t", err)
 		os.Exit(1)
 	}
-	resp, err := cli.ContainerCreate(context.Background(), &container.Config{
-		Image: "hello-world",
-		Tty:   true,
-	}, nil, nil, "")
-	if err != nil {
-		fmt.Println("Failed to run conainer due to:", err)
-		os.Exit(-1)
-	}
-	if err = cli.ContainerStart(context.Background(), resp.ID, types.ContainerStartOptions{}); err != nil {
-		fmt.Println("Failed to do the thing I love", err)
-		os.Exit(-1)
-	}
-	// Perform actions that are required once the image is running
-	t := 10 * time.Second
-	if err = cli.ContainerStop(context.Background(), resp.ID, &t); err != nil {
-		fmt.Println("Can not do it man:", err)
-		os.Exit(-1)
-	}
-	if err = cli.ContainerRemove(context.Background(), resp.ID, types.ContainerRemoveOptions{Force: true}); err != nil {
-		fmt.Println("I can not do that :", err)
-		os.Exit(-1)
+	defer func() {
+		if err := mchief.Stop(); err != nil {
+			fmt.Println("Failed to stop runner\n\t", err)
+			os.Exit(-1)
+		}
+	}()
+	for _, image := range os.Args[1:] {
+		if err = mchief.Start(image); err != nil {
+			fmt.Println("Failed to start the runner\n\t", err)
+			return
+		}
 	}
 }
--- runner/spartan.go
+++ runner/spartan.go
@@ -1,9 +1,16 @@
 package runner
 
 import (
+	"bufio"
 	"context"
+	"errors"
+	"fmt"
+	"io"
+	"os/exec"
 
 	"github.com/MovieStoreGuy/forerunner/config"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 )
 
@@ -13,6 +20,7 @@ type Spartan struct {
 	cli  *client.Client
 	ctx  context.Context
 	conf *config.Set
+	ids  []string
 }
 
 // New will create an instance of Spartan with the desired config
@@ -33,19 +41,56 @@ func New(conf *config.Set) (*Spartan, error) {
 
 // Start will run the docker image
 func (s *Spartan) Start(image string) error {
-
-	return nil
+	resp, err := s.cli.ContainerCreate(s.ctx, &container.Config{
+		Image: image,
+	}, nil, nil, "")
+	if err != nil {
+		return err
+	}
+	if err = s.cli.ContainerStart(s.ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		return err
+	}
+	s.ids = append(s.ids, resp.ID)
+	return s.runCommands(s.conf.Cmds...)
 }
 
 func (s *Spartan) runCommands(cmds ...[]string) error {
 	for _, cmd := range cmds {
-		_ = cmd
+		c := exec.Command(cmd[0], cmd[1:]...)
+		stdout, err := c.StdoutPipe()
+		if err != nil {
+			return errors.New("Unable to connect stdout")
+		}
+		stderr, err := c.StderrPipe()
+		if err != nil {
+			return errors.New("Unable to connect stderr")
+		}
+		writer := func(o io.ReadCloser) {
+			scanner := bufio.NewScanner(o)
+			for scanner.Scan() {
+				fmt.Println(scanner.Text())
+			}
+		}
+		// Write outputs as soon as they are received
+		go writer(stdout)
+		go writer(stderr)
+		if err = c.Run(); err != nil {
+			return err
+		}
 	}
 	return nil
 }
 
 // Stop will stop the given image from running and ensure that anything else
 // created with it is also cleaned up
-func (s *Spartan) Stop(image string) error {
+func (s *Spartan) Stop() error {
+	for _, id := range s.ids {
+		if err := s.cli.ContainerStop(s.ctx, id, nil); err != nil {
+			return err
+		}
+		if err := s.cli.ContainerRemove(s.ctx, id, types.ContainerRemoveOptions{Force: true}); err != nil {
+			return err
+		}
+	}
 	return nil
 }
